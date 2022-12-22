47f1129bec77cb0c864cd42ede6c66241eadc426
Kitisak <welovelove93@gmail.com>
2022-12-22 17:30:17 +0700

    Add project files.

---
+++ AUTO_START_DESDOCUMENT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AUTO_START_DESDOCUMENT.Services;
+
+namespace AUTO_START_DESDOCUMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutostartDocument.StartDoc();
+        }
+    }
+}
---
+++ AUTO_START_DESDOCUMENT/Services/AutostartDocument.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WolfApprove.Model.CustomClass;
+
+namespace AUTO_START_DESDOCUMENT.Services
+{
+    
+    class AutostartDocument
+    {
+        public static string connectionString = ConfigurationSettings.AppSettings["connectionString"];
+        
+        public static void StartDoc()
+        {
+            ISODBContextDataContext db = new ISODBContextDataContext(connectionString);
+            List<CustomJsonAdvanceForm.BoxLayout_RefDoc> tempMAdvanceFormItem = new List<CustomJsonAdvanceForm.BoxLayout_RefDoc>();
+            List<TRNMemo> listTrnMemo = new List<TRNMemo>();
+            var msttemplete = db.MSTTemplates.Where(x => x.DocumentCode == "DAR-N" && x.IsActive == true).ToList();
+            var SelectTemp = msttemplete.FirstOrDefault();
+            var trnmemo = db.TRNMemos.Where(x => x.TemplateId == SelectTemp.TemplateId).ToList();
+            foreach (var ItemMemo in trnmemo)
+            {
+                if (ItemMemo.MAdvancveForm.Contains("QS-F-IT-0005"))
+                {
+                    listTrnMemo.Add(ItemMemo);
+                }
+            }
+        }
+    }
+}
